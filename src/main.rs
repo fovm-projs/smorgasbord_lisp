1c54f180883a224c83b1b2f8bbe370126e1512c2
Culacov Alexandr <alex.10.03.2006@gmail.com>
2026-04-19 13:56:07 +0200

    CLI modification
    Now we can work with files

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,27 @@ use std::env;
 use parser::*;
 use backends::node;
 use std::io::Write;
+use std::io::Read;
 use std::fs::File;
 
 fn main() -> std::io::Result<()> {
-    let exm = "(write 0 (get_environment \"0\")) ; уже есть комментарии";
-    println!("{:?}", exm);
-    let code = node::transpile_program(&parse(exm));
-    println!("{:?}", code);
-
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-        let mut file = File::create(args[1].clone())?;
-        file.write_all(&code.into_bytes());
-    }
+
+    let code = if args.len() == 3 {
+        let input = std::fs::read_to_string(&args[1])?;
+        node::transpile_program(&parse(&input))
+    } else if args.len() == 2 {
+        let mut input = String::new();
+        std::io::stdin().read_to_string(&mut input)?;
+        node::transpile_program(&parse(&input))
+    } else {
+        eprintln!("Usage: prog [file_in] file_out");
+        std::process::exit(1);
+    };
+
+    let out_path = args.last().unwrap();
+    let mut file = File::create(out_path)?;
+    file.write_all(code.as_bytes())?;
 
     Ok(())
 }
